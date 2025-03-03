9912acf42078569cd2ff883f4c83730043478e63
Denis Domanskii <denidoman@outlook.com>
2025-03-03 20:49:58 +0100

    Cypress component test added

--- app/frontend/src/components/ClearChatButton/ClearChatButton.cy.tsx
+++ app/frontend/src/components/ClearChatButton/ClearChatButton.cy.tsx
@@ -4,6 +4,12 @@ import { ClearChatButton } from './ClearChatButton'
 describe('<ClearChatButton />', () => {
   it('renders', () => {
     // see: https://on.cypress.io/mounting-react
-    cy.mount(<ClearChatButton />)
+    cy.mount(
+        <ClearChatButton
+            onClick={function (): void {
+                throw new Error("Function not implemented.");
+            }}
+        />
+    );
   })
 })
